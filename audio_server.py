d1e4f6da164b56165292953189fb4008ea761e19
6cloudguy <pranav2308836@gmail.com>
2026-06-28 22:11:56 +0530

    python code updation

--- audio_server.py
+++ audio_server.py
@@ -1,29 +1,100 @@
-import flask
+#!/usr/bin/env python3
+
+import os
 import subprocess
+import flask
+from flask import stream_with_context
 
 app = flask.Flask(__name__)
 
-@app.route('/stream.mp3')
-def stream():
-    def generate():
-        proc = subprocess.Popen([
-            'ffmpeg', '-f', 'pulse',
-            '-i', 'alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink.monitor',
-            '-acodec', 'libmp3lame', '-ab', '128k', '-ar', '44100',
-            '-f', 'mp3', 'pipe:1'
-        ], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        
-        while True:
-            data = proc.stdout.read(4096)
-            if not data:
-                break
-            yield data
+MONITOR = os.environ.get(
+    "MONITOR",
+    "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink.monitor"
+)
 
-    response = flask.Response(generate(), mimetype='audio/mpeg')
-    response.headers['Content-Type'] = 'audio/mpeg'
-    response.headers['transferMode.dlna.org'] = 'Streaming'
-    response.headers['contentFeatures.dlna.org'] = 'DLNA.ORG_PN=MP3;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=0170000'
+FFMPEG_CMD = [
+    "ffmpeg",
+    "-nostdin",
+
+    "-f", "pulse",
+    "-i", MONITOR,
+
+    "-ac", "2",
+    "-ar", "44100",
+
+    "-codec:a", "libmp3lame",
+    "-b:a", "192k",
+
+    "-fflags", "+genpts",
+    "-flush_packets", "1",
+
+    "-f", "mp3",
+    "pipe:1",
+]
+
+
+@app.route("/stream.mp3")
+def stream():
+
+    def generate():
+
+        proc = subprocess.Popen(
+            FFMPEG_CMD,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            bufsize=0,
+        )
+
+        try:
+            while True:
+                chunk = proc.stdout.read(16384)
+
+                if not chunk:
+                    break
+
+                yield chunk
+
+        except GeneratorExit:
+            pass
+
+        finally:
+            if proc.poll() is None:
+                proc.kill()
+
+            proc.wait()
+
+    response = flask.Response(
+        stream_with_context(generate()),
+        mimetype="audio/mpeg",
+        direct_passthrough=True,
+    )
+
+    response.headers["Content-Type"] = "audio/mpeg"
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Connection"] = "keep-alive"
+
+    # DLNA headers
+    response.headers["transferMode.dlna.org"] = "Streaming"
+    response.headers["contentFeatures.dlna.org"] = (
+        "DLNA.ORG_PN=MP3;"
+        "DLNA.ORG_OP=01;"
+        "DLNA.ORG_FLAGS=01700000000000000000000000000000"
+    )
+
     return response
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+@app.route("/")
+def index():
+    return "Audio streaming server is running.\n"
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        threaded=False,
+        debug=False,
+        use_reloader=False,
+    )
